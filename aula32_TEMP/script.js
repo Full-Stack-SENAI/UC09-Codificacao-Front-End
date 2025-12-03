84fc12031a77b42bf4d7af5a6ebdda8b18e6a6e2
Magno V Gomes <magnovgomes74@gmail.com>
2025-12-03 18:33:22 -0300

    Removendo pastas travadas temporariamente

---
+++ aula32_TEMP/script.js
@@ -0,0 +1,12 @@
+// pegando o botão do arquivo html
+const botao = document.getElementById("btn");
+
+// pegando o 'h2' (subtitulo) do arquivo html
+const subtitulo = document.getElementById("subtitulo");
+
+// criando um ação para quando o botão for clicado
+botao.addEventListener("click", () => {
+    subtitulo.style.color = 'yellow';
+});
+
+// () => significa criar uma função, aula para o futuro do curso
